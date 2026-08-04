be77cd59f8b6476642eb744e999f95278340bb3e
Kell <gilsonvigilante@outlook.com>
2026-08-04 16:12:43 -0300

    feat: primeiro commit :D

---
+++ src/main/java/dev/kellyson/ImportaFlow/ImportaFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.kellyson.ImportaFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kellyson/ImportaFlow/ImportaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kellyson.ImportaFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
