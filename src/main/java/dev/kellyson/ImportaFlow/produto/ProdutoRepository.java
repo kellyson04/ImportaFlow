097d4e55432fe2fdfd85b28e5f80195e5644e747
Kell <gilsonvigilante@outlook.com>
2026-08-05 00:09:26 -0300

    feat: cria metodo existBySku

--- src/main/java/dev/kellyson/ImportaFlow/produto/ProdutoRepository.java
+++ src/main/java/dev/kellyson/ImportaFlow/produto/ProdutoRepository.java
@@ -3,4 +3,6 @@ package dev.kellyson.ImportaFlow.produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    boolean existsBySku(String sku);
 }
